841156c8a4344df0643648a8e11928ad91086420
Elijah Green <Elijah@3plsystems.com>
2019-09-27 22:37:20 -0700

    Change AudioComponent to class component

--- src/components/AudioComponent.tsx
+++ src/components/AudioComponent.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 import { toast } from "react-toastify";
+import { bindActionCreators, Dispatch } from "redux";
 import { ISong } from "../models";
 import Local from "../players/local";
 import {
@@ -8,122 +9,132 @@ import {
   setElapsed,
   toggleIsPlaying,
 } from "../store/reducers/songReducer";
-import { AppDispatch, AppState } from "../store/store";
+import { AppState } from "../store/store";
 
-const AudioComponent: React.FC = () => {
-  const playOnStartup = useSelector(
-    (state: AppState) => state.settings.playOnStartup,
-  );
-  const isPlaying = useSelector((state: AppState) => state.song.isPlaying);
-  const currentSong = useSelector((state: AppState) => state.song.currentSong);
-  const dispatch = useDispatch<AppDispatch>();
-  const volume = useSelector((state: AppState) => state.song.volume);
-  const muted = useSelector((state: AppState) => state.song.mute);
-  const firstRunRef = useRef(true);
-  const firstCurrentSongRef = useRef(true);
-  const firstIsPlayingRef = useRef(true);
+interface IProps extends StateProps, DispatchProps { }
 
-  const onError = useCallback(
-    (err: any, song?: ISong) => {
-      // NotAllowedError occurs when autoplay is denied.
-      if (err.name === "NotAllowedError") {
-        dispatch(setElapsed(0));
-        return;
-      }
+class AudioComponent extends React.Component<IProps, {}> {
+  private readonly local: Local;
+  constructor(props: IProps) {
+    super(props);
+    this.local = new Local(this.setTrackTimes, this.onSongEnd);
+  }
 
-      if (song) {
-        const message = `${song.name}: ${err.message}`;
-        toast.error(message);
-        // tslint:disable-next-line: no-console
-        console.log(err);
-      }
-
-      dispatch(nextTrack());
-    },
-    [dispatch],
-  );
-
-  const setTrackTimes = (currentTime: number, _: number) => {
-    dispatch(setElapsed(currentTime));
-  };
-
-  const onSongEnd = () => {
-    dispatch(nextTrack());
-  };
-
-  const local = useRef(new Local(setTrackTimes, onSongEnd));
-  const playSong = useCallback(
-    async (song: ISong) => {
-      try {
-        await local.current.play(song);
-      } catch (err) {
-        onError(err, song);
-      }
-    },
-    [onError],
-  );
-
-  // On App Start up
-  useEffect(() => {
-    if (firstRunRef.current && playOnStartup && isPlaying) {
-      if (currentSong) {
-        playSong(currentSong);
-      }
-    } else if (firstRunRef.current && isPlaying) {
-      dispatch(toggleIsPlaying());
+  public componentDidMount() {
+    if (this.props.playOnStartup && this.props.isPlaying) {
+      this.playCurrentSong();
+    } else if (this.props.isPlaying) {
+      this.props.toggleIsPlaying();
     }
-    firstRunRef.current = false;
-  }, [playOnStartup, isPlaying, currentSong, playSong, dispatch]);
+  }
 
-  // On new song
-  useEffect(() => {
-    const play = async () => {
-      local.current.pause();
-      if (currentSong) {
-        await playSong(currentSong);
-      }
-    };
-    if (!firstCurrentSongRef.current) {
-      play();
-    } else {
-      firstCurrentSongRef.current = false;
+  public async componentDidUpdate(prevProps: IProps) {
+    const currentProps = this.props;
+    await this.onCurrentSongUpdate(prevProps, currentProps);
+    await this.onIsPlayingUpdate(prevProps, currentProps);
+    this.onVolumeUpdate(prevProps, currentProps);
+    this.onMuteUpdate(prevProps, currentProps);
+  }
+
+  public render() {
+    return null;
+  }
+
+  private async onCurrentSongUpdate(prevProps: IProps, newProps: IProps) {
+    if (prevProps.currentSong && newProps.currentSong && prevProps.currentSong.id !== newProps.currentSong.id) {
+      await this.playSong(newProps.currentSong);
     }
-  }, [playSong, currentSong, dispatch]);
+  }
 
-  // On toggle play/pause
-  useEffect(() => {
-    const playPause = async () => {
-      if (isPlaying) {
-        local.current.resume();
-        if (!local.current.ready() && currentSong) {
-          await playSong(currentSong);
+  private async onIsPlayingUpdate(prevProps: IProps, newProps: IProps) {
+    if (prevProps.isPlaying !== newProps.isPlaying) {
+      if (newProps.isPlaying) {
+        this.local.resume();
+        if (!this.local.ready() && newProps.currentSong) {
+          await this.playSong(newProps.currentSong);
         }
       } else {
-        local.current.pause();
+        this.local.pause();
       }
-    };
-    if (!firstIsPlayingRef.current) {
-      playPause();
-    } else {
-      firstIsPlayingRef.current = false;
     }
-  }, [isPlaying, currentSong, playSong]);
+  }
 
-  // On volume change
-  useEffect(() => {
-    local.current.setVolume(volume);
-  }, [volume]);
-
-  // On mute
-  useEffect(() => {
-    if (muted) {
-      local.current.setVolume(0);
-    } else {
-      local.current.setVolume(volume);
+  private onVolumeUpdate(prevProps: IProps, newProps: IProps) {
+    if (prevProps.volume !== newProps.volume) {
+      this.local.setVolume(newProps.volume);
     }
-  }, [muted, volume]);
+  }
 
-  return null;
-};
+  private onMuteUpdate(prevProps: IProps, newProps: IProps) {
+    if (prevProps.mute !== newProps.mute) {
+      if (newProps.mute) {
+        this.local.setVolume(0);
+      } else {
+        this.local.setVolume(newProps.volume);
+      }
+    }
+  }
 
-export default AudioComponent;
+  private async playCurrentSong() {
+    const currentSong = this.props.currentSong;
+    if (currentSong) {
+      this.playSong(currentSong);
+    }
+  }
+  private setTrackTimes = (currentTime: number, _: number) => {
+    this.props.setElapsed(currentTime);
+  };
+
+  private onSongEnd = () => {
+    this.props.nextTrack();
+  };
+
+  private async playSong(song: ISong) {
+    if (song.from) {
+      try {
+        await this.local.play(song);
+      } catch (err) {
+        this.onError(err);
+        return;
+      }
+    }
+  }
+
+  private onError = (err: any) => {
+    // NotAllowedError occurs when autoplay is denied.
+    if (err.name === "NotAllowedError") {
+      setElapsed(0);
+      return;
+    }
+
+    if (this.props.currentSong) {
+      const message = `${this.props.currentSong.name}: ${err.message}`;
+      toast.error(message);
+      // tslint:disable-next-line: no-console
+      console.log(err);
+    }
+
+    this.props.nextTrack();
+  }
+}
+const mapStateToProps = (state: AppState) => ({
+  currentSong: state.song.currentSong,
+  elapsed: state.song.elapsed,
+  isPlaying: state.song.isPlaying,
+  mute: state.song.mute,
+  playOnStartup: state.settings.playOnStartup,
+  volume: state.song.volume,
+});
+type StateProps = ReturnType<typeof mapStateToProps>;
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      nextTrack,
+      setElapsed,
+      toggleIsPlaying
+    },
+    dispatch,
+  );
+type DispatchProps = ReturnType<typeof mapDispatchToProps>;
+export default connect(mapStateToProps, mapDispatchToProps)(AudioComponent);
