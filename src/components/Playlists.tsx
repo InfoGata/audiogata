3c5db9ee3c774a713a1903e560348f55919b492a
Elijah Green <elijahgreen@gmail.com>
2020-05-24 10:54:51 -0700

    And playlist delete button

--- src/components/Playlists.tsx
+++ src/components/Playlists.tsx
@@ -1,29 +1,75 @@
-import { List, ListItem, ListItemText } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  IconButton,
+} from "@material-ui/core";
 import React from "react";
 import { useSelector } from "react-redux";
 import { IPlaylist } from "../models";
-import { AppState } from "../store/store";
+import { AppState, AppDispatch } from "../store/store";
+import { Link } from "react-router-dom";
+import { Delete, MoreHoriz } from "@material-ui/icons";
+import { deletePlaylist } from "../store/reducers/playlistReducer";
+import { useDispatch } from "react-redux";
 
 interface IPlaylistsItemProps {
   playlist: IPlaylist;
+  openMenu: (event: React.MouseEvent<HTMLButtonElement>, playlist: IPlaylist) => void;
 }
 
 const PlaylistsItem: React.FC<IPlaylistsItemProps> = props => {
+  const { openMenu, playlist } = props;
+  const playlistPath = `/playlists/${props.playlist.id}`;
+  const openPlaylistMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    openMenu(event, playlist);
+  }
   return (
-    <ListItem>
+    <ListItem button={true} component={Link} to={playlistPath}>
       <ListItemText>{props.playlist.name}</ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton onClick={openPlaylistMenu}>
+          <MoreHoriz />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   );
 };
 
 const Playlists: React.FC = () => {
   const playlists = useSelector((state: AppState) => state.playlist.playlists);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [menuPlaylist, setMenuPlaylist] = React.useState<IPlaylist>({} as IPlaylist);
+  const closeMenu = () => setAnchorEl(null);
+  const dispatch = useDispatch<AppDispatch>();
+  const openMenu = (event: React.MouseEvent<HTMLButtonElement>, playlist: IPlaylist) => {
+    setAnchorEl(event.currentTarget);
+    setMenuPlaylist(playlist);
+  };
+  const deleteClick = () => {
+    dispatch(deletePlaylist(menuPlaylist));
+    closeMenu();
+  };
   return (
-    <List>
-      {playlists.map(p => (
-        <PlaylistsItem key={p.id} playlist={p} />
-      ))}
-    </List>
+    <>
+      <List>
+        {playlists.map(p => (
+          <PlaylistsItem key={p.id} playlist={p} openMenu={openMenu} />
+        ))}
+      </List>
+      <Menu open={Boolean(anchorEl)} onClose={closeMenu} anchorEl={anchorEl}>
+        <MenuItem onClick={deleteClick}>
+          <ListItemIcon>
+            <Delete />
+          </ListItemIcon>
+          <ListItemText primary="Delete" />
+        </MenuItem>
+      </Menu>
+    </>
   );
 };
 
