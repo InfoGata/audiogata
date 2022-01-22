6ee9c4c11a0f03b7b1cd27e94d382101d61b5586
Elijah Green <elijahgreen@gmail.com>
2022-01-21 23:05:37 -0800

    Fix config-overrides for new mui

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,20 @@
-const { fixBabelImports, override } = require('customize-cra')
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const { fixBabelImports, override } = require("customize-cra");
+const rewireReactHotLoader = require("react-app-rewire-hot-loader");
 
 module.exports = override(
   (config, env) => {
     if (env === "development") {
       config.resolve.alias["react-dom"] = "@hot-loader/react-dom";
     }
-    config = rewireReactHotLoader(config, env)
+    config = rewireReactHotLoader(config, env);
     return config;
   },
-  fixBabelImports('@material-ui/core', {
-    'libraryDirectory': 'esm',
-    'camel2DashComponentName': false
+  fixBabelImports("@mui/material", {
+    libraryDirectory: "",
+    camel2DashComponentName: false,
   }),
-  fixBabelImports('@material-ui/icons', {
-    'libraryDirectory': 'esm',
-    'camel2DashComponentName': false
-  }),
-)
+  fixBabelImports("@mui/icons-material", {
+    libraryDirectory: "",
+    camel2DashComponentName: false,
+  })
+);
