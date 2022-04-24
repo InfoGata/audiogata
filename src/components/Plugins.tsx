02e04eebf29242665509477c0c8cbae836fcb19e
Elijah Green <elijahgreen@gmail.com>
2022-04-23 19:09:16 -0700

    Allow loading of plugin from url

--- src/components/Plugins.tsx
+++ src/components/Plugins.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Button, Divider, Grid, styled } from "@mui/material";
+import { Button, Grid, styled, TextField, Typography } from "@mui/material";
 import { nanoid } from "@reduxjs/toolkit";
 import { FileType } from "../models";
 import { usePlugins } from "../PluginsContext";
 import PluginContainer from "./PluginContainer";
 import { directoryProps, getPlugin } from "../utils";
 
-const Input = styled("input")({
+const FileInput = styled("input")({
   display: "none",
 });
 
 const Plugins: React.FC = () => {
   const { plugins, addPlugin, deletePlugin } = usePlugins();
+  const [pluginUrl, setPluginUrl] = React.useState("");
+  const [headerKey, setHeaderKey] = React.useState("");
+  const [headerValue, setHeaderValue] = React.useState("");
 
   const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -20,6 +23,41 @@ const Plugins: React.FC = () => {
     const fileType: FileType = {
       filelist: files,
     };
+    const plugin = await getPlugin(fileType);
+
+    if (plugin) {
+      plugin.id = nanoid();
+      await addPlugin(plugin);
+    }
+  };
+
+  const onChangePluginUrl = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPluginUrl(event.target.value);
+  };
+  const onChangeHeaderKey = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHeaderKey(event.target.value);
+  };
+  const onChangeHeaderValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHeaderValue(event.target.value);
+  };
+
+  const onLoadUrl = async () => {
+    if (!pluginUrl.includes("manifest.json")) {
+      alert("The filename 'manifest.json' must be in the url");
+      return;
+    }
+    const headers = new Headers();
+    if (headerKey) {
+      headers.append(headerKey, headerValue);
+    }
+
+    const fileType: FileType = {
+      url: {
+        url: pluginUrl,
+        headers: headers,
+      },
+    };
+
     const plugin = await getPlugin(fileType);
 
     if (plugin) {
@@ -38,19 +76,42 @@ const Plugins: React.FC = () => {
 
   return (
     <Grid>
-      <label htmlFor="contained-button-file">
-        <Input
-          id="contained-button-file"
-          type="file"
-          {...directoryProps}
-          onChange={onFileChange}
+      <Grid>
+        <label htmlFor="contained-button-file">
+          <FileInput
+            id="contained-button-file"
+            type="file"
+            {...directoryProps}
+            onChange={onFileChange}
+          />
+          <Button variant="contained" component="span">
+            Load plugin From Folder
+          </Button>
+        </label>
+      </Grid>
+      <Grid>
+        <TextField
+          onChange={onChangePluginUrl}
+          value={pluginUrl}
+          placeholder="manifest.json url"
+          variant="outlined"
         />
-        <Button variant="contained" component="span">
-          Load plugin From Folder
-        </Button>
-      </label>
-      <Divider />
-      {pluginComponents}
+        <TextField
+          onChange={onChangeHeaderKey}
+          value={headerKey}
+          placeholder="Header key"
+          variant="outlined"
+        />
+        <TextField
+          onChange={onChangeHeaderValue}
+          value={headerValue}
+          placeholder="Header Value"
+          variant="outlined"
+        />
+        <Button onClick={onLoadUrl}>Load Url</Button>
+      </Grid>
+      <Typography></Typography>
+      <Grid>{pluginComponents}</Grid>
     </Grid>
   );
 };
