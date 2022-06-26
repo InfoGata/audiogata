b2be5a14dd6e040c264ac1bb0b3753d3ca9039e0
Elijah Green <elijahgreen@gmail.com>
2022-06-26 00:13:35 -0700

    Add more information to TrackInfo

--- src/components/TrackInfo.tsx
+++ src/components/TrackInfo.tsx
@@ -1,7 +1,8 @@
-import { Typography } from "@mui/material";
+import { List, ListItem, ListItemText } from "@mui/material";
 import React from "react";
 import { Track } from "../plugintypes";
 import { usePlugins } from "../PluginsContext";
+import { formatSeconds } from "../utils";
 
 interface TrackInfoProps {
   track: Track;
@@ -13,8 +14,26 @@ const TrackInfo: React.FC<TrackInfoProps> = (props) => {
   const plugin = plugins.find((p) => p.id === track.pluginId);
   return (
     <>
-      <Typography>{track.name}</Typography>
-      <Typography>{plugin?.name}</Typography>
+      <List>
+        <ListItem>
+          <ListItemText primary="Track name" secondary={track.name} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="ID" secondary={track.id} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Plugin" secondary={plugin?.name} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="API ID" secondary={track.apiId} />
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary="Duration"
+            secondary={formatSeconds(track.duration)}
+          />
+        </ListItem>
+      </List>
     </>
   );
 };
