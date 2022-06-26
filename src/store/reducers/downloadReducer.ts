36a8ab521494c5198be6d72233b684bb339419f3
Elijah Green <elijahgreen@gmail.com>
2022-06-26 00:27:30 -0700

    Remove native and extension downloading for now

--- src/store/reducers/downloadReducer.ts
+++ src/store/reducers/downloadReducer.ts
@@ -2,7 +2,6 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AudioBlob, db } from "../../database";
 import { Track } from "../../plugintypes";
 import { AppThunk } from "../store";
-import { Capacitor } from "@capacitor/core";
 
 interface TrackProgress {
   trackId: string;
@@ -52,51 +51,40 @@ export const downloadTrack =
     let response: Response | undefined = undefined;
     let blob: Blob;
     try {
-      if (Capacitor.isNativePlatform()) {
-        // TODO: add progress when using mobile
-        response = await window.cordovaFetch(url);
-        blob = await response.blob();
-      } else if (typeof window.MediaGata !== "undefined") {
-        // TODO: Implement downloads when using MediaGata extension
-        const extensionResponse = await window.MediaGata.networkRequest(url);
-        blob = extensionResponse.body;
-        dispatch(downloadsSlice.actions.downloadFailure(track.id || ""));
-      } else {
-        try {
-          response = await fetch(url);
-        } catch {}
-        // Error maybe because of cors so try a the proxy
-        if (!response) {
-          response = await fetch(`http://localhost:8085/${url}`);
-        }
-        const reader = response.body?.getReader();
-        if (response.headers.has("Content-Length") && reader) {
-          const contentLenghStr = response.headers.get("Content-Length") || "";
-          const contentLength = +contentLenghStr;
-          let receivedLength = 0;
-          let chunks: Uint8Array[] = [];
-          while (true) {
-            const { done, value } = await reader.read();
+      try {
+        response = await fetch(url);
+      } catch {}
+      // Error maybe because of cors so try a the proxy
+      if (!response) {
+        response = await fetch(`http://localhost:8085/${url}`);
+      }
+      const reader = response.body?.getReader();
+      if (response.headers.has("Content-Length") && reader) {
+        const contentLenghStr = response.headers.get("Content-Length") || "";
+        const contentLength = +contentLenghStr;
+        let receivedLength = 0;
+        let chunks: Uint8Array[] = [];
+        while (true) {
+          const { done, value } = await reader.read();
 
-            if (done) {
-              break;
-            }
-
-            chunks.push(value || new Uint8Array());
-            receivedLength += value?.length || 0;
-            const progress = (receivedLength / contentLength) * 100;
-            dispatch(
-              downloadsSlice.actions.setDownloadProgress({
-                trackId: track.id || "",
-                progress,
-              })
-            );
+          if (done) {
+            break;
           }
 
-          blob = new Blob(chunks);
-        } else {
-          blob = await response.blob();
+          chunks.push(value || new Uint8Array());
+          receivedLength += value?.length || 0;
+          const progress = (receivedLength / contentLength) * 100;
+          dispatch(
+            downloadsSlice.actions.setDownloadProgress({
+              trackId: track.id || "",
+              progress,
+            })
+          );
         }
+
+        blob = new Blob(chunks);
+      } else {
+        blob = await response.blob();
       }
       const audioBlob: AudioBlob = {
         id: track.id || "",
