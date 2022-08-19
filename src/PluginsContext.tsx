070c2f2678c5a6f531d2a5da9cfc69f43eefeffc
Elijah Green <elijahgreen@gmail.com>
2022-08-18 17:54:37 -0700

    Use put instead of update for updating plugins

--- src/PluginsContext.tsx
+++ src/PluginsContext.tsx
@@ -433,7 +433,7 @@ export const PluginsProvider: React.FC<React.PropsWithChildren> = (props) => {
     oldPlugin?.destroy();
     const pluginFrame = await loadPlugin(plugin, pluginFiles);
     setPluginFrames(pluginFrames.map((p) => (p.id === id ? pluginFrame : p)));
-    await db.plugins.update(id, plugin);
+    await db.plugins.put(plugin);
   };
 
   const deletePlugin = async (pluginFrame: PluginFrameContainer) => {
