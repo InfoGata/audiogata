bc9c3d7c84dab4081d9a1c00d4e0d92da2d78078
Elijah Green <elijahgreen@gmail.com>
2022-09-06 17:42:13 -0700

    Add greetings text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import PluginCards from "./PluginCards";
 import TopItemCards from "./TopItemCards";
@@ -5,6 +6,7 @@ import TopItemCards from "./TopItemCards";
 const Home: React.FC = () => {
   return (
     <>
+      <Typography variant="h4">Greetings</Typography>
       <TopItemCards />
       <PluginCards />
     </>
