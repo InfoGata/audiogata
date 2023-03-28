7faef85fc82f84980d6e722361a29b871551e559
Elijah Green <elijah@infogata.com>
2023-03-27 19:15:53 -0700

    Change capacitor webDir to dist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.audiogata.app',
-  appName: 'AudioGata',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "com.audiogata.app",
+  appName: "AudioGata",
+  webDir: "dist",
+  bundledWebRuntime: false,
 };
 
 export default config;
