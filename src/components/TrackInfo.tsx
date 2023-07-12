7b43f8a055aa6f4cab9ac26124e869b6f1e7d49b
Elijah Green <elijah@infogata.com>
2023-07-11 17:50:40 -0700

    Add track art to info page

--- src/components/TrackInfo.tsx
+++ src/components/TrackInfo.tsx
@@ -1,7 +1,17 @@
-import { List, ListItem, ListItemText } from "@mui/material";
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
 import React from "react";
 import { Track } from "../plugintypes";
-import { formatSeconds } from "../utils";
+import {
+  formatSeconds,
+  getThumbnailImage,
+  searchThumbnailSize,
+} from "../utils";
 import { useTranslation } from "react-i18next";
 import usePlugins from "../hooks/usePlugins";
 
@@ -14,6 +24,7 @@ const TrackInfo: React.FC<TrackInfoProps> = (props) => {
   const { plugins } = usePlugins();
   const { t } = useTranslation();
   const plugin = plugins.find((p) => p.id === track.pluginId);
+  const image = getThumbnailImage(track.images, searchThumbnailSize);
 
   return (
     <>
@@ -40,6 +51,15 @@ const TrackInfo: React.FC<TrackInfoProps> = (props) => {
           <ListItemText
             primary={t("trackDuration")}
             secondary={formatSeconds(track.duration)}
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar alt={track.name} src={image} style={{ borderRadius: 0 }} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={t("trackArt")}
+            secondary={track.images?.map((i) => i.url).join(", ")}
           />
         </ListItem>
       </List>
