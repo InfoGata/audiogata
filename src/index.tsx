4f156b79ec00e6fc75407de75768f3c9afe856f5
Elijah Green <elijah@infogata.com>
2023-10-16 11:13:43 -0700

    Add glitchtip error tracking

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import * as Sentry from "@sentry/browser";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -11,6 +12,10 @@ import App from "./App";
 import "./i18n";
 import "./index.css";
 import store, { persistor } from "./store/store";
+
+Sentry.init({
+  dsn: "https://d99bb253ac5a4b53a32d48697f165e34@app.glitchtip.com/4798",
+});
 
 const theme = createTheme({
   palette: {
