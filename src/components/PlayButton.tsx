738395abfcde8c50f79a993349340be243658271
Elijah Green <elijah@infogata.com>
2024-05-15 20:49:20 -0700

    Set classes of PlayButton to important

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -9,7 +9,7 @@ const PlayButton: React.FC<PlayButtonProps> = (props) => {
   const { onClick } = props;
   return (
     <Button variant="ghost" size="icon" className="w-16 h-16" onClick={onClick}>
-      <MdPlayCircle className="w-14 h-14" />
+      <MdPlayCircle className="!w-14 !h-14" />
     </Button>
   );
 };
