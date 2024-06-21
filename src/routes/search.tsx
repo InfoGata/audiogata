f5323011d2a7b61969cf140f0425845e992e0aa0
Elijah Green <elijah@infogata.com>
2024-06-20 21:12:56 -0700

    Fix issues with showing search

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,7 +1,15 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { createFileRoute } from "@tanstack/react-router";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useQuery, useQueryClient } from "react-query";
+import { z } from "zod";
+import AlbumSearchResults from "../components/AlbumSearchResults";
+import ArtistSearchResults from "../components/ArtistSearchResults";
+import PlaylistSearchResults from "../components/PlaylistSearchResults";
+import SelectPlugin from "../components/SelectPlugin";
+import Spinner from "../components/Spinner";
+import TrackSearchResults from "../components/TrackSearchResults";
 import usePlugins from "../hooks/usePlugins";
 import {
   Album,
@@ -11,14 +19,6 @@ import {
   Track,
 } from "../plugintypes";
 import { SearchResultType } from "../types";
-import AlbumSearchResults from "../components/AlbumSearchResults";
-import ArtistSearchResults from "../components/ArtistSearchResults";
-import PlaylistSearchResults from "../components/PlaylistSearchResults";
-import SelectPlugin from "../components/SelectPlugin";
-import Spinner from "../components/Spinner";
-import TrackSearchResults from "../components/TrackSearchResults";
-import { Tabs, TabsContent, TabsTrigger } from "@/components/ui/tabs";
-import { z } from "zod";
 
 const Search: React.FC = () => {
   const [pluginId, setPluginId] = React.useState("");
@@ -85,63 +85,65 @@ const Search: React.FC = () => {
         useCurrentPlugin={true}
       />
       <Tabs value={tabValue} onValueChange={handleChange}>
-        {trackList.length > 0 ? (
-          <TabsTrigger value={SearchResultType.Tracks} className="flex-1">
-            {t("tracks")}
-          </TabsTrigger>
-        ) : null}
-        {albumList.length > 0 ? (
-          <TabsTrigger value={SearchResultType.Albums}>
-            {t("albums")}
-          </TabsTrigger>
-        ) : null}
-        {artistList && artistList.length > 0 ? (
-          <TabsTrigger value={SearchResultType.Artists}>
-            {t("artists")}
-          </TabsTrigger>
-        ) : null}
-        {playlistList && playlistList.length > 0 ? (
-          <TabsTrigger value={SearchResultType.Playlists}>
-            {t("playlists")}
-          </TabsTrigger>
-        ) : null}
+        <TabsList className="flex w-full justify-around">
+          {trackList.length > 0 ? (
+            <TabsTrigger value={SearchResultType.Tracks} className="flex-1">
+              {t("tracks")}
+            </TabsTrigger>
+          ) : null}
+          {albumList.length > 0 ? (
+            <TabsTrigger value={SearchResultType.Albums} className="flex-1">
+              {t("albums")}
+            </TabsTrigger>
+          ) : null}
+          {artistList && artistList.length > 0 ? (
+            <TabsTrigger value={SearchResultType.Artists} className="flex-1">
+              {t("artists")}
+            </TabsTrigger>
+          ) : null}
+          {playlistList && playlistList.length > 0 ? (
+            <TabsTrigger value={SearchResultType.Playlists} className="flex-1">
+              {t("playlists")}
+            </TabsTrigger>
+          ) : null}
+        </TabsList>
+        <TabsContent value={SearchResultType.Tracks}>
+          {trackList.length > 0 ? (
+            <TrackSearchResults
+              pluginId={pluginId}
+              searchQuery={q}
+              initialPage={query.data?.tracks?.pageInfo}
+            />
+          ) : null}
+        </TabsContent>
+        <TabsContent value={SearchResultType.Albums}>
+          {albumList.length > 0 ? (
+            <AlbumSearchResults
+              pluginId={pluginId}
+              searchQuery={q}
+              initialPage={query.data?.albums?.pageInfo}
+            />
+          ) : null}
+        </TabsContent>
+        <TabsContent value={SearchResultType.Artists}>
+          {artistList.length > 0 ? (
+            <ArtistSearchResults
+              pluginId={pluginId}
+              searchQuery={q}
+              initialPage={query.data?.artists?.pageInfo}
+            />
+          ) : null}
+        </TabsContent>
+        <TabsContent value={SearchResultType.Playlists}>
+          {playlistList.length > 0 ? (
+            <PlaylistSearchResults
+              pluginId={pluginId}
+              searchQuery={q}
+              initialPage={query.data?.playlists?.pageInfo}
+            />
+          ) : null}
+        </TabsContent>
       </Tabs>
-      <TabsContent value={tabValue}>
-        {trackList.length > 0 ? (
-          <TrackSearchResults
-            pluginId={pluginId}
-            searchQuery={q}
-            initialPage={query.data?.tracks?.pageInfo}
-          />
-        ) : null}
-      </TabsContent>
-      <TabsContent value={tabValue}>
-        {albumList.length > 0 ? (
-          <AlbumSearchResults
-            pluginId={pluginId}
-            searchQuery={q}
-            initialPage={query.data?.albums?.pageInfo}
-          />
-        ) : null}
-      </TabsContent>
-      <TabsContent value={tabValue}>
-        {artistList.length > 0 ? (
-          <ArtistSearchResults
-            pluginId={pluginId}
-            searchQuery={q}
-            initialPage={query.data?.artists?.pageInfo}
-          />
-        ) : null}
-      </TabsContent>
-      <TabsContent value={tabValue}>
-        {playlistList.length > 0 ? (
-          <PlaylistSearchResults
-            pluginId={pluginId}
-            searchQuery={q}
-            initialPage={query.data?.playlists?.pageInfo}
-          />
-        ) : null}
-      </TabsContent>
     </>
   );
 };
