2cb5d4b31e08df40e4ea2f0631e4226127bc952a
Elijah Green <elijah@infogata.com>
2024-10-08 20:00:13 -0700

    Disable enabling chatbot for now

--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -6,7 +6,7 @@ import ShowForwardAndRewind from "@/components/Settings/ShowForwardAndRewind";
 import ThemeChangeSetting from "@/components/Settings/ThemeChangeSetting";
 import UpdateCorsSetting from "@/components/Settings/UpdateCorsSettings";
 import React from "react";
-import ChatBotSetting from "@/components/Settings/ChatBotSetting";
+// import ChatBotSetting from "@/components/Settings/ChatBotSetting";
 
 const Settings: React.FC = () => {
   return (
@@ -17,7 +17,7 @@ const Settings: React.FC = () => {
       <UpdateCorsSetting />
       <SelectLyricsPlugin />
       <ThemeChangeSetting />
-      <ChatBotSetting />
+      {/* <ChatBotSetting /> */}
     </div>
   );
 };
