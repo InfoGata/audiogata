1f4db9c7b933ebbb48bea3b36e83ef6163db71aa
Elijah Green <elijah@infogata.com>
2025-08-15 21:40:09 -0700

    feat: Introduce ArtistTopTracks component to display top tracks on artist page
    - Create ArtistTopTracks component to fetch and render top tracks using React Query
    - Integrate ArtistTopTracks into the artist page, replacing previous TrackList implementation
    - Implement pagination and selection functionality for top tracks
    - Update artist info handling to reflect changes in top tracks data

---
+++ src/components/ArtistTopTracks.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { useTranslation } from "react-i18next";
+import TrackList from "@/components/TrackList";
+import Pager from "@/components/Pager";
+import useSelected from "@/hooks/useSelected";
+import usePagination from "@/hooks/usePagination";
+import { Artist, Track, PageInfo } from "@/plugintypes";
+import { useAppDispatch } from "@/store/hooks";
+import { setTrack, setTracks } from "@/store/reducers/trackReducer";
+
+interface ArtistTopTracksProps {
+  pluginId: string;
+  apiId: string;
+  plugin: any;
+  pluginsLoaded: boolean;
+  onArtistInfoUpdate?: (artist: Artist) => void;
+}
+
+const ArtistTopTracks: React.FC<ArtistTopTracksProps> = ({
+  pluginId,
+  apiId,
+  plugin,
+  pluginsLoaded,
+  onArtistInfoUpdate,
+}) => {
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+
+  const [currentPage, setCurrentPage] = React.useState<PageInfo>();
+  const { page, hasNextPage, hasPreviousPage, onPreviousPage, onNextPage } =
+    usePagination(currentPage);
+
+  const onGetTopTracks = async () => {
+    if (plugin && (await plugin.hasDefined.onGetArtistTopTracks())) {
+      const topTracksData = await plugin.remote.onGetArtistTopTracks({
+        apiId: apiId,
+        pageInfo: page,
+      });
+      if (topTracksData.artist && onArtistInfoUpdate) {
+        onArtistInfoUpdate(topTracksData.artist);
+      }
+      setCurrentPage(topTracksData.pageInfo);
+      return topTracksData.items;
+    }
+    return [];
+  };
+
+  const topTracksQuery = useQuery(
+    ["artisttoptracks", pluginId, apiId, page],
+    onGetTopTracks,
+    {
+      enabled: pluginsLoaded && !!plugin,
+    }
+  );
+
+  const topTracks = topTracksQuery.data || [];
+  const { onSelect, onSelectAll, isSelected, selected } = useSelected(topTracks);
+
+  const onTrackClick = (track: Track) => {
+    dispatch(setTracks(topTracks));
+    dispatch(setTrack(track));
+  };
+
+  if (topTracks.length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <h2 className="text-2xl font-bold mb-4">{t("topTracks")}</h2>
+      <TrackList
+        tracks={topTracks}
+        onTrackClick={onTrackClick}
+        onSelect={onSelect}
+        isSelected={isSelected}
+        onSelectAll={onSelectAll}
+        selected={selected}
+        dragDisabled={true}
+      />
+      <Pager
+        hasNextPage={hasNextPage}
+        hasPreviousPage={hasPreviousPage}
+        onPreviousPage={onPreviousPage}
+        onNextPage={onNextPage}
+      />
+    </>
+  );
+};
+
+export default ArtistTopTracks;
--- src/routes/plugins/$pluginId/artists.$apiId.tsx
+++ src/routes/plugins/$pluginId/artists.$apiId.tsx
@@ -9,14 +9,11 @@ import ConfirmPluginDialog from "@/components/ConfirmPluginDialog";
 import Pager from "@/components/Pager";
 import PlaylistInfoCard from "@/components/PlaylistInfoCard";
 import Spinner from "@/components/Spinner";
-import TrackList from "@/components/TrackList";
+import ArtistTopTracks from "@/components/ArtistTopTracks";
 import useFindPlugin from "@/hooks/useFindPlugin";
 import usePagination from "@/hooks/usePagination";
 import usePlugins from "@/hooks/usePlugins";
-import useSelected from "@/hooks/useSelected";
-import { Artist, PageInfo, Track } from "@/plugintypes";
-import { useAppDispatch } from "@/store/hooks";
-import { setTrack, setTracks } from "@/store/reducers/trackReducer";
+import { Artist, PageInfo } from "@/plugintypes";
 
 const ArtistPage: React.FC = () => {
   const { pluginId, apiId } = Route.useParams();
@@ -25,7 +22,6 @@ const ArtistPage: React.FC = () => {
   const [artistInfo, setArtistInfo] = React.useState<Artist | undefined>(
     state.artist
   );
-  const dispatch = useAppDispatch();
   const { t } = useTranslation();
   const plugin = plugins.find((p) => p.id === pluginId);
   const { isLoading, pendingPlugin, removePendingPlugin } = useFindPlugin({
@@ -56,38 +52,15 @@ const ArtistPage: React.FC = () => {
     enabled: pluginsLoaded && !!plugin,
   });
 
-  const onGetTopTracks = async () => {
-    if (plugin && (await plugin.hasDefined.onGetArtistTopTracks())) {
-      const topTracksData = await plugin.remote.onGetArtistTopTracks({
-        apiId: apiId,
-      });
-      if (topTracksData.artist && !artistInfo) {
-        setArtistInfo(topTracksData.artist);
-      }
-      return topTracksData.items;
+  const handleArtistInfoUpdate = (artist: Artist) => {
+    if (!artistInfo) {
+      setArtistInfo(artist);
     }
-    return [];
-  };
-
-  const topTracksQuery = useQuery(
-    ["artisttoptracks", pluginId, apiId],
-    onGetTopTracks,
-    {
-      enabled: pluginsLoaded && !!plugin,
-    }
-  );
-
-  const topTracks = topTracksQuery.data || [];
-  const { onSelect, onSelectAll, isSelected, selected } = useSelected(topTracks);
-
-  const onTrackClick = (track: Track) => {
-    dispatch(setTracks(topTracks));
-    dispatch(setTrack(track));
   };
 
   return (
     <>
-      <Spinner open={query.isLoading || topTracksQuery.isLoading || isLoading} />
+      <Spinner open={query.isLoading || isLoading} />
       {artistInfo && (
         <PlaylistInfoCard
           name={artistInfo.name || ""}
@@ -97,20 +70,13 @@ const ArtistPage: React.FC = () => {
       {artistInfo && (
         <ItemMenu itemType={{ type: "artist", item: artistInfo }} />
       )}
-      {topTracks.length > 0 && (
-        <>
-          <h2 className="text-2xl font-bold mb-4">{t("topTracks")}</h2>
-          <TrackList
-            tracks={topTracks}
-            onTrackClick={onTrackClick}
-            onSelect={onSelect}
-            isSelected={isSelected}
-            onSelectAll={onSelectAll}
-            selected={selected}
-            dragDisabled={true}
-          />
-        </>
-      )}
+      <ArtistTopTracks
+        pluginId={pluginId}
+        apiId={apiId}
+        plugin={plugin}
+        pluginsLoaded={pluginsLoaded}
+        onArtistInfoUpdate={handleArtistInfoUpdate}
+      />
       <h2 className="text-2xl font-bold mb-4 mt-8">{t("albums")}</h2>
       <CardContainer>
         {query.data?.map((a) => <AlbumCard key={a.id} album={a} noArtist={true} />)}
